c45fe7cbc92a4ad15831d762e9549e5f1048d7d6
Dormio <alex.kellermann@live.com>
2019-04-12 17:57:38 -0400

    Added HTML selectors and frontend stuff

--- db/www/newModel.php
+++ db/www/newModel.php
@@ -1,128 +1,146 @@
 <?php
-# Author: Alexander Kellermann Nieves
-# Date: April 9th, 2019
-# Summary:
-# This file contains the model that's used to identify APTs on the server
-
-
 # Set up the credentials for the MySQL server
-# If you've changed this for your install, you'll need to change these usename
-# and passwords. It would be useful if you sourced these from a config file
-# that you've set up. This is here just as a reference
-$password="hang6Chien1";
-$user="db";
-$database="adapt";
+$password = "hang6Chien1";
+$user = "db";
+$database = "adapt";
 
-# Use the recommended interface to connect with the database. In order for this
-# to work properly you need to make sure that the file is running on the same server
-# that the MySQL DB is on. Otherwise all you'll need to change is the IP address
-# from 127.0.0.1 to the new IP Address.
+# Set up the connection to the server (which should exist on the local host)
+
 $mysqli = new mysqli("127.0.0.1", $user, $password, $database);
 
-
-
-# Store the name of the APTs
-$apt_name = [];
-
-# I don't remember
-$phases = [];
-
-# I also don't remember
-$apts2 = [];
-
-# We're running this query to make sure that the model will have every technique
-# regardless of whether it's observered or not. If we don't do this, we can run
-# into errors. This came up during testing.
-$query="SELECT * FROM attack_patterns";
-
-# Run the query and save the results to $results
-$results = $mysqli->query("$query");
-
-# Going through each row of the returns
-while($row = $results->fetch_assoc()){
-
-    # Save the name of the apt
-    $testVar = $row['Name'];
-    
-    # Save the phase it's from
-    $apt_name = $row['Phase'];
-    
-    # TODO: I don't think we need this anymore. Try removing.
-	array_push($apts2, $apt_name);
-	if (!array_key_exists($testVar, $phases)){
-		$phases[$testVar] = 1;
-	} else {
-        $phases[$testVar] += 1;
-	}
-}
-
-$array_of_techniques = array_keys($phases);
-
-# Get the name of all the groups from the groups_and_techniques table.
-# Remember, this table is not generated from the load-data.php like the
-# other tables are. You need to run the script from ./PythonRandoms/stixx_gen.py 
-# which will output a file similar to groups_techniques.txt 
-# Then use the load-groups.php to load it to the database.
-$query = "SELECT `groupname` FROM `groups_and_techniques`";
+# Build a list of distinct computer names that we might want to run the model
+# on
+$query = "SELECT DISTINCT ComputerName FROM patterns_seen";
 $results = $mysqli->query($query);
-$aptnames = [];
 
-while($row = $results->fetch_assoc()){
-    array_push($aptnames, $row['groupname']);
+# Here I end the php code to insert HTML in order to prepare the form.
+?>
+<h3> Select the Computers you want to run the model against below </h3>
+<form action="#" method="post">
+
+    <?php
+    while ($row = $results->fetch_assoc()) :
+        ?>
+
+        <input type="checkbox" name="computers[]" value="<?php echo $row['ComputerName']; ?>"><?php echo $row['ComputerName']; ?></input></br>
+
+    <?php endwhile; ?>
+    <input type="submit" name="submit" value="Submit" />
+</form>
+<?php
+
+# When the model is first accessed by webpage, there haven't been any
+# checkboxes selected, so we don't need to run the model against anything.
+if(isset($_POST['computers'])){
+    run_model($_POST['computers'], $mysqli);
 }
 
-# Lets create the model right here
-$model = [];
-$apts = [];
+# this is the beginning of the function that runs the model. It takes two parameters
+# param $array_of_computers
+# Array of the computers: e.g., WINDOWS1 WINDOWS2
+# param $mysqli
+# the mysqli created out of this scope. That way we don't need to recreate it.
+function run_model($array_of_computers, $mysqli)
+{
 
-foreach($array_of_techniques as $indi_tech){
-       $model[$indi_tech] =[];
-}
+    # Store the name of the APTs
+    $apt_name = [];
+    $phases = [];
+    $apts2 = [];
+    # We're running this query to make sure that the model will have every technique
+    # regardless of whether it's observered or not. If we don't do this, we can run
+    # into errors. This came up during testing.
+    $query = "SELECT * FROM attack_patterns";
+    $results = $mysqli->query("$query");
+    while ($row = $results->fetch_assoc()) {
+        $testVar = $row['Name'];
+        $apt_name = $row['Phase'];
+        array_push($apts2, $apt_name);
+        if (!array_key_exists($testVar, $phases)) {
+            $phases[$testVar] = 1;
+        }
+    }
 
+    # Now we have all the possible techniques.
+    $array_of_techniques = array_keys($phases);
 
-foreach($aptnames as $name){
-    $query = "select techniques from groups_and_techniques where groupname = '$name'";
+    # Get the name of all the groups from the groups_and_techniques table.
+    # Remember, this table is not generated from the load-data.php like the
+    # other tables are. You need to run the script from ./PythonRandoms/stixx_gen.py 
+    # which will output a file similar to groups_techniques.txt 
+    # Then use the load-groups.php to load it to the database.
+    $query = "SELECT `groupname` FROM `groups_and_techniques`";
     $results = $mysqli->query($query);
-    $apts[$name] = 0.0; # Use floats because we're going to be doing fractions.
+    $aptnames = [];
 
-    while($row = $results->fetch_assoc()){
-        # explode works similarly to .split() in python
-        # basically we get a string of 'techniques' that are delimited by
-        # commas, since I want all of these individually, I explode them.
-        $techs = explode(',', $row['techniques']);
+    while ($row = $results->fetch_assoc()) {
+        array_push($aptnames, $row['groupname']);
+    }
 
-        foreach($techs as $things){
-            # Inside of here, we know what APT we're looking at
-            # As well as the techniques, which will be represented
-            # in the $things variable.
-            if (array_key_exists($things, $model)){
-                array_push($model[$things], $name);
-            } else {
-                # TODO: 
-                # We probably don't need either of these if statements
-                # because this was created before we put the technique builder
-                $model[$things] = [];
-                array_push($model[$things], $name);
+    # Lets create the model right here
+    $model = [];
+    $apts = [];
+
+    foreach ($array_of_techniques as $indi_tech) {
+        $model[$indi_tech] = [];
+    }
+
+
+    foreach ($aptnames as $name) {
+
+        $query = "select techniques from groups_and_techniques where groupname = '$name'";
+        $results = $mysqli->query($query);
+        $apts[$name] = 0.0; # Use floats because we're going to be doing fractions.
+
+        while ($row = $results->fetch_assoc()) {
+            # explode works similarly to .split() in python
+            # basically we get a string of 'techniques' that are delimited by
+            # commas, since I want all of these individually, I explode them.
+            $techs = explode(',', $row['techniques']);
+
+            foreach ($techs as $things) {
+                # Inside of here, we know what APT we're looking at
+                # As well as the techniques, which will be represented
+                # in the $things variable.
+                if (array_key_exists($things, $model)) {
+                    array_push($model[$things], $name);
+                } else {
+                    # TODO: 
+                    # We probably don't need either of these if statements
+                    # because this was created before we put the technique builde
+                    $model[$things] = [];
+                    array_push($model[$things], $name);
+                }
             }
         }
     }
-}
-
-# patterns_seen is the table of the database that has all the events that sysmon
-# has seen. This is when we use the model to classify the most likely APT
-$query = "SELECT * FROM patterns_seen";
-$results = $mysqli->query("$query");
-
-while($row = $results->fetch_assoc()){
-    foreach($model[$row['Name']] as $ind_apts){
-        $apts[$ind_apts] += (1/count($model[$row['Name']]));
+    # $array_of_computers contains all the computers we want to run against the WHERE clause.
+    # The next block of code just constructs the MYSQL query we want to use. It looks confusing
+    # but it's just a series of string concats.
+    $long_string = "WHERE ComputerName = '" . array_pop($array_of_computers) . "'";
+    foreach($array_of_computers as $computer_name){
+        $long_string .= " or ComputerName = " . " '" . $computer_name . "'";
     }
-}
+    $long_string .= ";";
 
-# arsort sorts the keys in the 2D array by the value, in descending order.
-# that way when we var_dump it, the most likely APT appears at the top.
-arsort($apts);
-var_dump($apts);
 
+    $query = "SELECT * FROM patterns_seen " . $long_string;
+    echo "Query ran:" . '</br>';
+    echo $query . '</br>';
+    $results = $mysqli->query("$query");
+
+    # Here's where the actual mathematics goes on. Also where the program spends the majority
+    # of its time. If you're looking to change the model, this is a good place to start.
+    while ($row = $results->fetch_assoc()) {
+        foreach ($model[$row['Name']] as $ind_apts) {
+            $apts[$ind_apts] += (1 / count($model[$row['Name']]));
+        }
+    }
+    
+    # arsort sorts the keys in the 2D array by the value, in descending order.
+    # that way when we var_dump it, the most likely APT appears at the top.
+    arsort($apts);
+    var_dump($apts);
+} # This is the end of the function
 
 ?>
