18ed0b8097f5e7123a1c4225e403a57fbe19165a
Kristen Tumacder <kmt5215@rit.edu>
2019-04-15 21:10:21 -0400

    Update generate_noise.ps1
    Added log source. This works!

--- Tests/generate_noise.ps1
+++ Tests/generate_noise.ps1
@@ -1,16 +1,22 @@
+#[System.Diagnostics.EventLog]::LogNameFromSourceName('Windows2','.')
+#[System.Diagnostics.EventLog]::SourceExists('Windows2')
+
+# Uncomment this line when doing this for the first time
+#New-EventLog -LogName Application -Source 'Test'
+
 
 # Successful logon - event ID 4624
-Write-EventLog -LogName "Security" -EventId 4624 -Message "noise - user successful logon"
+Write-EventLog -LogName Application -Source 'Test' -EventId 4624 -Message "noise - user successful logon"
 
 # Failed logon - event ID 4625
-Write-EventLog -LogName "Security" -EventId 4625 -Message "noise - user failed logon"
+Write-EventLog -LogName Application -Source 'Test' -EventId 4625 -Message "noise - user failed logon"
 
 # File was deteleted - event ID 5141
-Write-EventLog -LogName "Security" -EventId 5141 -Message "noise - file deleted"
+Write-EventLog -LogName Application -Source 'Test' -EventId 5141 -Message "noise - file deleted"
 
 # File in AD was created - event ID 5137
-Write-EventLog -LogName "Security" -EventId 5137 -Message "noise - file created" 
+Write-EventLog -LogName Application -Source 'Test' -EventId 5137 -Message "noise - file created" 
 
 # File permissions - event ID 4670
-Write-EventLog -LogName "Security" -EventId 4670 -Message "noise - file permissions"
+Write-EventLog -LogName Application -Source 'Test' -EventId 4670 -Message "noise - file permissions"
  
